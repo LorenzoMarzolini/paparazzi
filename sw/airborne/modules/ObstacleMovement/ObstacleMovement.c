c8675cc19dd5e935b192549a5aa01d6eac9b913a
LorenzoMarzolini <lorenzo.marzolini@gmail.com>
2025-03-14 20:29:39 +0100

    Update ObstacleMovement.c

--- sw/airborne/modules/ObstacleMovement/ObstacleMovement.c
+++ sw/airborne/modules/ObstacleMovement/ObstacleMovement.c
@@ -16,6 +16,8 @@
 #include "state.h"
 #include "modules/core/abi.h"
 
+extern float oag_max_speed; 
+extern float oag_heading_rate;
 // Navigation state enum definitions
 enum navigation_state_t {
     HOLD,
