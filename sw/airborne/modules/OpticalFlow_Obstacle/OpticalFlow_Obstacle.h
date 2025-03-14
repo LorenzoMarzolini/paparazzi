6b3eaebddb9ca29d317cee7f66c58dcd09da943f
LorenzoMarzolini <lorenzo.marzolini@gmail.com>
2025-03-14 23:24:03 +0100

    Update OpticalFlow_Obstacle.h

--- sw/airborne/modules/OpticalFlow_Obstacle/OpticalFlow_Obstacle.h
+++ sw/airborne/modules/OpticalFlow_Obstacle/OpticalFlow_Obstacle.h
@@ -12,6 +12,10 @@
 #ifndef OPTICAL_FLOW_OBSTACLE_H
 #define OPTICAL_FLOW_OBSTACLE_H
 
+
+extern float oag_max_speed; 
+extern float oag_heading_rate;
+
 // Initialize the optical flow obstacle detection module
 extern void optical_flow_obs_init(void);
 
